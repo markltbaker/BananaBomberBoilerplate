8e1f9b860123901bed5c5dd5ff871b5a9421be73
Mark Baker <markb@mindcandy.com>
2012-11-04 14:40:28 +0000

    cleaning up world generation

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -6,6 +6,12 @@ Crafty.scene("main", function() {
         "src/entities/Player2.js",
     ];
 
+    // utility for map generator -- create 2D entities on a grid
+    function gridEntity(i, j, z, components) {
+        return Crafty.e("2D, " + gameContainer.conf.get('renderType') + ", " + components)
+            .attr({ x: i * 16, y: j * 16, z:z });
+    }
+
     //method to generate the map
     function generateWorld() {
         //loop through all tiles
@@ -14,26 +20,23 @@ Crafty.scene("main", function() {
 
                 //place grass on all tiles
                 grassType = Crafty.math.randomInt(1, 4);
-                Crafty.e("2D, "+gameContainer.conf.get('renderType')+", grass" + grassType)
-                    .attr({ x: i * 16, y: j * 16, z:1 });
+                gridEntity(i, j, 1, "grass" + grassType);
+
                 //grid of bushes
                 if((i % 2 === 0) && (j % 2 === 0)) {
-                    Crafty.e("2D, "+gameContainer.conf.get('renderType')+", solid, bush1")
-                        .attr({x: i * 16, y: j * 16, z: 2000})
+                    gridEntity(i,j, 2000, "solid, bush1");
                 }
 
                 //create a fence of bushes
                 if(i === 0 || i === 24 || j === 0 || j === 20)
-                    Crafty.e("2D, "+gameContainer.conf.get('renderType')+", solid, bush" + Crafty.math.randomInt(1, 2))
-                    .attr({ x: i * 16, y: j * 16, z: 2 });
+                    gridEntity(i,j,2, "solid, bush" + Crafty.math.randomInt(1, 2))
 
                 //generate some nice flowers within the boundaries of the outer bushes
                 if (i > 0 && i < 24 && j > 0 && j < 20
                         && Crafty.math.randomInt(0, 50) > 30
                         && !(i === 1 && j >= 16)
                         && !(i === 23 && j <= 4)) {
-                    var f = Crafty.e("2D, "+gameContainer.conf.get('renderType')+", flower, Collision, solid, SpriteAnimation, explodable")
-                            .attr({ x: i * 16, y: j * 16, z: 1000 })
+                    var f = gridEntity(i,j,1000, "flower, Collision, solid, SpriteAnimation, explodable")
                             .animate("wind", 0, 1, 3)
                             .animate('wind', 80, -1)
                             .bind('explode', function() {
